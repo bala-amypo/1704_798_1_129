4ac3142ca71308a4c6d6c48b99348042ba580406
bala-amypo <bala@amypo.in>
2025-12-19 06:13:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Product;
+
+public interface ProductService {
+
+    Product createProduct(Product product);
+
+    Product getProductBySku(String sku);
+}
